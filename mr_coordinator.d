f79bc67d06a9faade25a05919109d830cd36445b
HuiD <391678792hd@sjtu.edu.cn>
2021-12-23 14:57:54 +0800

    make tidy

--- mr_coordinator.d
+++
@@ -1,3 +0,0 @@
-mr_coordinator.o: mr_coordinator.cc mr_protocol.h rpc/rpc.h \
- rpc/thr_pool.h rpc/fifo.h rpc/slock.h lang/verify.h rpc/marshall.h \
- lang/algorithm.h rpc/connection.h rpc/pollmgr.h
--- rpc/thr_pool.d
+++
@@ -1,2 +0,0 @@
-rpc/thr_pool.o: rpc/thr_pool.cc rpc/slock.h lang/verify.h rpc/thr_pool.h \
- rpc/fifo.h
